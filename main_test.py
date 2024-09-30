261d47d9b1ef6da09493746785e47e1c16f2eec0
Kevin Maurizi <kevin.maurizi@bzz.ch>
2024-09-30 09:04:43 +0200

    Ran pyink on branch main

--- main_test.py
+++ main_test.py
@@ -6,18 +6,20 @@ def test_add():
     assert add(0, 0) == 0
     assert add(-5, 5) == 0
 
+
 def test_subtract():
     assert subtract(10, 5) == 5
     assert subtract(0, 0) == 0
     assert subtract(-5, 5) == -10
+
 
 def test_multiply():
     assert multiply(3, 4) == 12
     assert multiply(0, 5) == 0
     assert multiply(-3, 4) == -12
 
+
 def test_divide():
     assert divide(15, 3) == 5
     assert divide(0, 5) == 0
     assert divide(15, 0) == "Division durch Null ist nicht erlaubt!"
-
